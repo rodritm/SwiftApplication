a38b3f6164fd49e349525798cd5d527090b3ba68
rodritm <rodritm2010@gmail.com>
2018-12-27 11:44:42 -0400

    Initial Commit

---
+++ TextMessageApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TextMessageApp
+//
+//  Created by Rodrigo Tejada on 12/27/18.
+//  Copyright © 2018 Rodrigo Tejada. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
